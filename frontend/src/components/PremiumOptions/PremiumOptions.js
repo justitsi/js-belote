13e1b60f92acc7cac8dd17c294c930b4af3c3e64
Hristo Belchev <hristob1999@gmail.com>
2021-04-11 11:22:10 +0100

    Make deselecting available premiums possible :P

--- frontend/src/components/PremiumOptions/PremiumOptions.js
+++ frontend/src/components/PremiumOptions/PremiumOptions.js
@@ -6,103 +6,117 @@ import { useTranslation } from 'react-i18next';
 function PremiumOptions(props) {
     const { t } = useTranslation('translations');
     const [selectedCPremiums, setSelectedCPremiums] = useState([])
+    const [preselectedCPremiums, setPreselectedCPremiums] = useState(false)
     const [selectedSPremiums, setSelectedSPremiums] = useState([])
+    const [preselectedSPremiums, setPreselectedSPremiums] = useState(false)
     const availablePremiumsList = []
     let cIndex = 0;
     let sIndex = 0;
 
-    const handlePremiumClick = (premiumType, index) => {
-        let list
+    if (props.roundStatus) {
+        // preselect all premiums being rendered
+        if (preselectedCPremiums === false && props.availablePremiums.C.length > 0) {
+            const list = [...selectedCPremiums]
+            for (let i = 0; i < props.availablePremiums.C.length; i++)
+                list.push(i)
 
-        if (premiumType === 'C') list = [...selectedCPremiums]
-        if (premiumType === 'S') list = [...selectedSPremiums]
-
-        // deselect if index is in arr
-        if (list.includes(index)) list.splice(list.indexOf(index), 1);
-        // select if index is not in arr 
-        else list.push(index)
-
-        if (premiumType === 'C') setSelectedCPremiums(list)
-        if (premiumType === 'S') setSelectedSPremiums(list)
-    }
-
-    const handleAnouncePremiumsClick = () => {
-        const premiumsToAnounce = []
-
-        for (const index of selectedCPremiums) {
-            const premium = {
-                cards: props.availablePremiums.C[index],
-                type: 'C'
-            }
-            premiumsToAnounce.push(premium)
-        }
-
-        for (const index of selectedSPremiums) {
-            const premium = {
-                cards: props.availablePremiums.S[index],
-                type: 'S'
-            }
-            premiumsToAnounce.push(premium)
-        }
-        setSelectedCPremiums([])
-        setSelectedSPremiums([])
-        // console.log(`anouncing premiums ${premiumsToAnounce}`)
-        props.handleAnouncePremiums(premiumsToAnounce)
-    }
-
-    for (const cardSeriesOption of props.availablePremiums.C) {
-        let option = (
-            <div
-                className={styles.premiumsListItem}
-                key={cIndex}
-            >
-                <PremiumOption
-                    premiumCards={cardSeriesOption}
-                    premiumType="C"
-                    selected={selectedCPremiums.includes(cIndex)}
-                    index={cIndex}
-                    handleClick={handlePremiumClick}
-                />
-            </div>
-        );
-        // preselect all indexes being rendered
-        const list = [...selectedCPremiums]
-        if (!list.includes(cIndex)) {
-            list.push(cIndex)
             setSelectedCPremiums(list)
+            setPreselectedCPremiums(true)
         }
+        if (preselectedSPremiums === false && props.availablePremiums.S.length > 0) {
+            const list = [...selectedSPremiums]
+            for (let i = 0; i < props.availablePremiums.S.length; i++)
+                list.push(i)
 
-        cIndex++;
-        availablePremiumsList.push(option)
-    }
-
-    for (const cardSeriesOption of props.availablePremiums.S) {
-        let option = (
-            <div
-                className={styles.premiumsListItem}
-                key={sIndex}
-            >
-                <PremiumOption
-                    premiumCards={cardSeriesOption}
-                    premiumType="S"
-                    selected={selectedSPremiums.includes(sIndex)}
-                    index={sIndex}
-                    handleClick={handlePremiumClick}
-                />
-            </div>
-        );
-        // preselect all indexes being rendered
-        const list = [...selectedSPremiums]
-        if (!list.includes(sIndex)) {
-            list.push(sIndex)
             setSelectedSPremiums(list)
+            setPreselectedSPremiums(true)
         }
 
-        sIndex++;
-        availablePremiumsList.push(option)
-    }
+        const handlePremiumClick = (premiumType, index) => {
+            let list
 
-    if (props.roundStatus)
+            if (premiumType === 'C') list = [...selectedCPremiums]
+            if (premiumType === 'S') list = [...selectedSPremiums]
+
+            // deselect if index is in arr
+            if (list.includes(index)) list.splice(list.indexOf(index), 1);
+            // select if index is not in arr 
+            else list.push(index)
+
+            if (premiumType === 'C') setSelectedCPremiums(list)
+            if (premiumType === 'S') setSelectedSPremiums(list)
+        }
+
+        const handleAnouncePremiumsClick = () => {
+            const premiumsToAnounce = []
+
+            for (const index of selectedCPremiums) {
+                const premium = {
+                    cards: props.availablePremiums.C[index],
+                    type: 'C'
+                }
+                premiumsToAnounce.push(premium)
+            }
+
+            for (const index of selectedSPremiums) {
+                const premium = {
+                    cards: props.availablePremiums.S[index],
+                    type: 'S'
+                }
+                premiumsToAnounce.push(premium)
+            }
+            setSelectedCPremiums([])
+            setSelectedSPremiums([])
+            // console.log(`anouncing premiums ${premiumsToAnounce}`)
+            props.handleAnouncePremiums(premiumsToAnounce)
+        }
+
+        for (const cardSeriesOption of props.availablePremiums.C) {
+            let option = (
+                <div
+                    className={styles.premiumsListItem}
+                    key={cIndex}
+                >
+                    <PremiumOption
+                        premiumCards={cardSeriesOption}
+                        premiumType="C"
+                        selected={selectedCPremiums.includes(cIndex)}
+                        index={cIndex}
+                        handleClick={handlePremiumClick}
+                    />
+                </div>
+            );
+            cIndex++;
+            availablePremiumsList.push(option)
+        }
+
+        for (const cardSeriesOption of props.availablePremiums.S) {
+            let option = (
+                <div
+                    className={styles.premiumsListItem}
+                    key={sIndex}
+                >
+                    <PremiumOption
+                        premiumCards={cardSeriesOption}
+                        premiumType="S"
+                        selected={selectedSPremiums.includes(sIndex)}
+                        index={sIndex}
+                        handleClick={handlePremiumClick}
+                    />
+                </div>
+            );
+            // preselect all indexes being rendered
+            const list = [...selectedSPremiums]
+            if (!list.includes(sIndex)) {
+                list.push(sIndex)
+                setSelectedSPremiums(list)
+            }
+
+            sIndex++;
+            availablePremiumsList.push(option)
+        }
+
+
         return (
             <div>
                 {availablePremiumsList.length > 0 && props.roundStatus.pTurnName === props.displayName &&
@@ -121,6 +135,7 @@ function PremiumOptions(props) {
                 }
             </div >
         )
+    }
     else {
         return (
             <div />
