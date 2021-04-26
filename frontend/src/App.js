25df5455df9457b84711f43141399dd478b8acae
Hristo Belchev <hristob1999@gmail.com>
2021-04-26 16:58:43 +0100

    add footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,27 @@ import i18n from './i18n';
 import { I18nextProvider } from 'react-i18next';
 import MainPage from './content/MainPage';
 import BelotePage from './content/BelotePage';
-import Navbar from './components/SiteComponents/Navbar'
+import Navbar from './components/SiteComponents/Navbar';
+import Footer from './components/SiteComponents/Footer';
 
 function App() {
   return (
     <div className={styles.App}>
       <I18nextProvider i18n={i18n}>
-        <Navbar />
-        <HashRouter>
-          <Route exact
-            path='/'
-            component={MainPage}
-          />
-          <Route exact
-            path='/belote/:roomID'
-            component={BelotePage}
-          />
-        </HashRouter>
+        <div className={styles.content}>
+          <Navbar />
+          <HashRouter>
+            <Route exact
+              path='/'
+              component={MainPage}
+            />
+            <Route exact
+              path='/belote/:roomID'
+              component={BelotePage}
+            />
+          </HashRouter>
+        </div>
+        <Footer />
       </I18nextProvider>
     </div>
   );
---
+++ frontend/src/components/SiteComponents/Footer/Footer.js
@@ -0,0 +1,78 @@
+import styles from './Footer.module.scss'
+import { Row, Col, Button, Jumbotron } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next';
+import signature from './../../../assets/images/logo.png';
+
+function Footer(props) {
+    const { t } = useTranslation('translations');
+
+    return (
+        <Jumbotron className={styles.footerContainer}>
+            {/* <Row>
+                <Col sm={3} md={4} lg={4} />
+                <Col sm={6} md={4} lg={4}>
+                    <p className={styles.moreInfoLabel}>
+                        {t('footer.moreInfoLabel')}
+                    </p>
+                </Col>
+            </Row>
+            <br /> */}
+            <Row>
+                <Col sm={0} md={1} lg={2} />
+                <Col sm={12} md={10} lg={8}>
+                    <Row>
+                        <Col md={3}>
+                            <p>
+                                {t('footer.cookieStatement1')}
+                                <br />
+                                {t('footer.cookieStatement2')}
+                            </p>
+                        </Col>
+                        <Col md={6}>
+                            <p>
+                                {t('footer.copyright')}
+                                <a href={"https://github.com/justitsi/js-belote/blob/master/LICENSE"}>
+                                    {t('footer.copyrightLinkLabel')}
+                                </a>
+                            </p>
+
+                            <p>
+                                {t('footer.githubLinkExplanation')}
+                            </p>
+                            <a href={"https://github.com/justitsi/js-belote/issues/new/choose"}>
+                                <Button>
+                                    {t('footer.githubLinkLabel')}
+                                </Button>
+                            </a>
+                        </Col>
+                        <Col md={3}>
+                            <a href={"https://justitsi.tk"}>
+                                <img
+                                    className={styles.signature}
+                                    src={signature}
+                                    alt="Logo"
+                                />
+                            </a>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Jumbotron >
+    );
+}
+export default Footer;
+
+/*
+    Footer layout:
+    SM:
+    Col1
+    Col2
+    Col3
+
+    MD:
+    Col1 Col2
+    Col3
+
+    >=LG:
+    Col1 Col2 Col3
+*/
