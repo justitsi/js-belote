6d508a3ab70b37fad3ee39cf6d811294f43874bc
justitsi <hristob1999@gmail.com>
2021-04-27 11:04:37 +0100

    Add bulgarian locale

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,6 +1,8 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from './locales/en.json';
+import translationBG from './locales/bg.json';
+
 
 i18n.use(LanguageDetector).init({
     fallback: 'en',
@@ -8,6 +10,9 @@ i18n.use(LanguageDetector).init({
     resources: {
         en: {
             translations: translationEN,
+        },
+        bg: {
+            translations: translationBG,
         }
     },
     ns: ['translations'],
