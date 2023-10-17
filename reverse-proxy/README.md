a33e30776320f45bcf7a017e1ec996fe81bc3d6d
Hristo Belchev <hristob1999@gmail.com>
2023-10-17 19:41:23 +0100

    Add Docker Compose based deployment for service

---
+++ reverse-proxy/README.md
@@ -0,0 +1,6 @@
+# Belote service reverse proxy
+
+This folder contains the files required to build the reverse-proxy nginx for the internal network.
+
+Important note:
+HTTPS is to be provided by external web server!
---
+++ startup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git switch deploy
+
+docker compose build
+docker compose up -d
+
+# remove dangling images as per 
+# https://stackoverflow.com/questions/50126741/how-to-remove-intermediate-images-from-a-build-after-the-build
+ docker rmi -f $(docker images -f "dangling=true" -q)
