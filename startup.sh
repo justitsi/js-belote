e46b7ca255971d4724bb147faefd9eb00e1557de
Hristo Belchev <hristob1999@gmail.com>
2023-10-21 14:04:36 +0100

    Switch to using host builds for frontend

--- startup.sh
+++ startup.sh
@@ -8,3 +8,8 @@ docker compose up -d
 # remove dangling images as per 
 # https://stackoverflow.com/questions/50126741/how-to-remove-intermediate-images-from-a-build-after-the-build
  docker rmi -f $(docker images -f "dangling=true" -q)
+
+#  still building frontend locally as networking issues cause npm ci to fail in docker
+cd frontend/
+npm ci
+npm run build
